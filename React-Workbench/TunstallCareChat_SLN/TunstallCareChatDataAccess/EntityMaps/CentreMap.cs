2ee3978f9d7aa31ace3fe1a04a990fab3664de11
José Luis del Álamo García-Dorado <jlalamo@tunstall.es>
2018-11-01 09:48:45 +0100

    Added control centre and terminals component corrected.

---
+++ React-Workbench/TunstallCareChat_SLN/TunstallCareChatDataAccess/EntityMaps/CentreMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TunstallCareChatDataAccess.Models;
+
+namespace TunstallCareChatDataAccess.EntityMaps
+{
+    public class CentreMap : IEntityTypeConfiguration<CentreModel>
+    {
+        public void Configure(EntityTypeBuilder<CentreModel> builder)
+        {
+            builder.ToTable("TIB_CENTRE_DEF", "dbo");            
+        }
+    }
+}
